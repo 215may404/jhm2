49cc9039aa85a28f5644f33dd849c3016b4a9995
215may404 <yipmay215@gmail.com>
2025-01-11 13:09:16 +0800

    Update expensetracker.py

--- ExpenseTracker/expensetracker.py
+++ ExpenseTracker/expensetracker.py
@@ -1,6 +1,13 @@
 import pandas as pd
-import csv
-print("""
+columns = ["date","type","description","amount",]
+try:
+    df = pd.read_csv('expenses.csv')
+except FileNotFoundError:
+    df = pd.DataFrame(columns = columns)
+    print('No file, please build a new file!')
+filename = 'expenses.csv'
+while True:
+    print("""
 Personal Expense Tracker
   1. Add Transaction
   2. Edit Transaction 
@@ -8,25 +15,42 @@ Personal Expense Tracker
   4. View Summary
   5. Save and Exit
   """)
-
-description = ""
-amount = ""
-date_input = ""
-expense_df = pd.DataFrame(columns=["Date","Description","Amount"])
-choice = ""
-while choice != "5":
-    choice = input("Enter your choice: ")
+    choice = input("Please enter your choice (1-5): ")   
     if choice == "1":
-       description = input("Enter description of expense: ")
-       amount = input("Enter amount: ")
-       date_input = input("Enter date of expense (YYYY-MM-DD): ") 
-       expense_df = pd.read_csv("expense.csv")
+        "date" == input("Enter date of expense (YYYY-MM-DD): ")
+        "type" == input("Enter type (expenses/income): ")
+        "description" == input("Enter description of expense: ") 
+        "amount" == float(input("Enter amount: "))
+        df.loc[len(df)] = {'Date':"date",'Description':"description",'Amount':"amount",'Type':"type"} 
+    
     elif choice == "2":
         print("edit transaction")
+        index = int(input("enter the edited tag:"))
+        print(df.loc[index])
+        new_date = input('New Date(enter original date):')
+        new_descripition = input('New Descripitopn(enter original descripition):')
+        new_amount = input('New Amount(enter original amount):')
+        new_type = input('New type(enter original type): ')
+        df.loc[index] = {'date':new_date,'description':new_descripition,'amount':new_amount,'type':new_type}
+        
     elif choice == "3":
         print("delete transaction")
+        index = int(input("enter the deleted tag:"))
+        df = df.drop(index).reset_index(drop =True)
     elif choice == "4":
-        print("Expense: ")
-        print(expense_df)
-    else:
+        total_expenses = df[df['type'] == 'expenses']['amount'].sum()
+        total_income = df[df['type'] == 'income']['amount'].sum()
+        if total_expenses == 0 and total_income == 0:print('no related record')
+        else:
+            print("records")
+            savings = total_income - total_expenses
+            print(f'toal expenses:{total_expenses}')
+            print(f'total income:{total_income}')
+            print(f'balance:{savings}')
+        
+    elif choice == "5":
+        df.to_csv(filename, index=False)
         print("save and exit")
+        break
+    else:    
+        print("Invalid choice, please try agian.")
