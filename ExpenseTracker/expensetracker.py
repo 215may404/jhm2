712feece61daa8640ef067efa1558c0a91b7751e
215may404 <yipmay215@gmail.com>
2025-01-18 12:58:55 +0800

    Update expensetracker.py

--- ExpenseTracker/expensetracker.py
+++ ExpenseTracker/expensetracker.py
@@ -1,11 +1,12 @@
 import pandas as pd
-columns = ["date","type","description","amount",]
+columns = ['Date','Type','Description','Amount']
+filename = 'expenses.csv' 
 try:
-    df = pd.read_csv('expenses.csv')
+    df = pd.read_csv(filename)
 except FileNotFoundError:
     df = pd.DataFrame(columns = columns)
     print('No file, please build a new file!')
-filename = 'expenses.csv'
+
 while True:
     print("""
 Personal Expense Tracker
@@ -17,37 +18,42 @@ Personal Expense Tracker
   """)
     choice = input("Please enter your choice (1-5): ")   
     if choice == "1":
-        "date" == input("Enter date of expense (YYYY-MM-DD): ")
-        "type" == input("Enter type (expenses/income): ")
-        "description" == input("Enter description of expense: ") 
-        "amount" == float(input("Enter amount: "))
-        df.loc[len(df)] = {'Date':"date",'Description':"description",'Amount':"amount",'Type':"type"} 
-    
+        date = input("Enter date of expense (YYYY-MM-DD): ")
+        trans_type = input("Enter type (expenses/income): ")
+        description = input("Enter description of expense: ") 
+        amount = float(input("Enter amount: "))
+        df.loc[len(df)] = {'Date':date,'Type':trans_type,'Description':description,'Amount':amount} 
+        df.to_csv(filename, index=False)
+        
     elif choice == "2":
         print("edit transaction")
+        print(df)
         index = int(input("enter the edited tag:"))
         print(df.loc[index])
-        new_date = input('New Date(enter original date):')
-        new_descripition = input('New Descripitopn(enter original descripition):')
-        new_amount = input('New Amount(enter original amount):')
-        new_type = input('New type(enter original type): ')
-        df.loc[index] = {'date':new_date,'description':new_descripition,'amount':new_amount,'type':new_type}
-        
+        new_date = input('New Date(Enter 保留原值):') or df.loc[index,'Date']
+        new_type = input('New type(Enter 保留原值): ') or df.loc[index,'Type']
+        new_descripition = input('New Descripitopn(Enter 保留原值):') or df.loc[index,'Description']
+        new_amount = input('New Amount(Enter 保留原值):') or df.loc[index,'Amount']
+        df.loc[index] = {'Date':new_date,'Type':new_type,'Description':new_descripition,'Amount':float(new_amount)}
+        df.to_csv(filename, index=False)   
     elif choice == "3":
         print("delete transaction")
         index = int(input("enter the deleted tag:"))
         df = df.drop(index).reset_index(drop =True)
+        df.to_csv(filename, index=False)
     elif choice == "4":
-        total_expenses = df[df['type'] == 'expenses']['amount'].sum()
-        total_income = df[df['type'] == 'income']['amount'].sum()
+        print(df)
+
+        total_expenses = df[df['Type'] == 'expenses']['Amount'].sum()
+        total_income = df[df['Type'] == 'income']['Amount'].sum()
+        savings = total_income - total_expenses
         if total_expenses == 0 and total_income == 0:print('no related record')
         else:
             print("records")
-            savings = total_income - total_expenses
             print(f'toal expenses:{total_expenses}')
             print(f'total income:{total_income}')
             print(f'balance:{savings}')
-        
+
     elif choice == "5":
         df.to_csv(filename, index=False)
         print("save and exit")
