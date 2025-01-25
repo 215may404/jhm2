5365e42f3e6abb3f1a39c3d095b0e6a400ce51d0
215may404 <yipmay215@gmail.com>
2025-01-25 12:17:02 +0800

    Create YIP Mei Wa Q1.py

---
+++ YIP Mei Wa Q1.py
@@ -0,0 +1,10 @@
+X = input(Enter a price:)
+Y = input(Enter another price:)
+if X > Y:
+  print('The first price is larger than the second one.')
+elif X = Y:
+  print('The price are the same.')
+elif X < Y:
+  print('The first price is smaller than the second one.')
+else:
+  print('Error')
