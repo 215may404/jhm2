ed2dbd54dd396d1bee86327f12930bbffd27fd2f
215may404 <yipmay215@gmail.com>
2025-02-08 01:40:42 +0800

    Update YIP Mei Wa Q4.py

--- YIP Mei Wa Q4.py
+++ YIP Mei Wa Q4.py
@@ -1,17 +1,34 @@
-while is true:
-a = float(input("Input the height of a student 1 in cm:"))
-b = float(input("Input the height of a student 2 in cm:"))
-c = float(input("Input the height of a student 3 in cm:"))
-d = float(input("Input the height of a student 4 in cm:"))
-  if a or b or c or d < 0:
-    print("Height must be positive."）
-  elif a or b or c or d > 200:
-    print("Height is greater than 200cm.")
-  elif 0 < a or b or c or d > 200:
-    print("End of input.")
-    break
-  else：
-    print("Error")
-values = [a, b, c, d]
+while True:
+    a = float(input("Input the height of a student 1 in cm:")),
+    if a < 0:
+        print("Height must be positive.")
+    elif a > 200:
+        print("Height is greater than 200cm.")
+    else:
+        print("Error")
+    b = float(input("Input the height of a student 2 in cm:")),
+    if b < 0:
+        print("Height must be positive.")
+    elif b > 200:
+        print("Height is greater than 200cm.")
+    else:
+        print("Error")
+    c = float(input("Input the height of a student 3 in cm:")),
+    if c < 0:
+        print("Height must be positive.")
+    elif c > 200:
+        print("Height is greater than 200cm.")
+    else:
+        print("Error")
+    d = float(input("Input the height of a student 4 in cm:")),
+    if d < 0:
+        print("Height must be positive.")
+    elif d > 200:
+        print("Height is greater than 200cm.")
+    else:
+        print("Error")
+        break
+values = [a,b,c,d]
+print("End of input.")
 print("The average height of these students is {float(average(values))} cm.")
 print("The maximum height of these students is {float(max(values))} cm.")
