58a2da1d22ad5b31572a49cd83e03c1813c60a5c
215may404 <yipmay215@gmail.com>
2025-02-08 00:51:35 +0800

    Update YIP Mei Wa Q3.py

--- YIP Mei Wa Q3.py
+++ YIP Mei Wa Q3.py
@@ -1,1 +1,4 @@
-input("Input Addition Table Size Smaller 10:")
+range_limit = int(input("Input Addition Table Size smaller 10:"))
+for i in range(1, range_limit + 1):
+    for j in range(1, range_limit + 1):
+        print(f"{i + j:2}",end="")
