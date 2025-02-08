295530c2022f48d6d77dc609e7987932cce8e774
215may404 <yipmay215@gmail.com>
2025-02-08 10:21:07 +0800

    Update YIP Mei Wa Q3.py

--- YIP Mei Wa Q3.py
+++ YIP Mei Wa Q3.py
@@ -1,4 +1,4 @@
-range_limit = int(input("Input Addition Table Size smaller 10:"))
-for i in range(1, range_limit + 1):
-    for j in range(1, range_limit + 1):
-        print(f"{i + j:2}",end="")
+size = int(input("Input Addition Table Size smaller 10:"))
+for i in range(size):
+    for j in range(size):
+    print(f"{i+1} + {j+1} = {i+1+j+1}:2")
