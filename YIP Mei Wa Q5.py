069f6bf199424e4fb5a1a814a8544a7a9cf11da2
215may404 <yipmay215@gmail.com>
2025-02-08 10:21:39 +0800

    Update YIP Mei Wa Q5.py

--- YIP Mei Wa Q5.py
+++ YIP Mei Wa Q5.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib pyplot as plt
+import matplotlib.pyplot as plt
 data = {
   'Month' : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
   'Sales' : [380,400,660,800,900,1200,1600,2200,1500,1100,600,250]
@@ -7,7 +7,7 @@ data = {
 df = pd.DataFrame(data)
 filename = 'sales.csv'
 df.to_csv(filename, index = False)
-plt.bar(Month,Sales,color = 'green',wide = 0.5)
+plt.bar("Month","Sales",color = 'green',wide = 0.5)
 plt.title("Bar Chart of Ice-cream sales")
 plt.xlabel("Month")
 plt.ylabel("Sales(in thousand dollars)") 
