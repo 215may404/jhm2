3a49e17cf6c0dbd38f2158ad23021d30d9f085e1
215may404 <yipmay215@gmail.com>
2025-02-15 11:25:54 +0800

    Update YIP Mei Wa Q5.py

--- YIP Mei Wa Q5.py
+++ YIP Mei Wa Q5.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 data = {
-  'Month' : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-  'Sales' : [380,400,660,800,900,1200,1600,2200,1500,1100,600,250]
+    'Month' : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    'Sales' : [380,400,660,800,900,1200,1600,2200,1500,1100,600,250]
 }
 df = pd.DataFrame(data)
 filename = 'sales.csv'
 df.to_csv(filename, index = False)
-plt.bar("Month","Sales",color = 'green',wide = 0.5)
+
+try:
+  loaded_df = pd.read_csv(filename)
+  print(loaded_df)
+except FileNotFoundError:
+  print(f"\nfile does not exist.")
+  
+month = loaded_df['Month']
+sales = loaded_df['Sales']
+plt.bar(month,sales,color = 'green',width=0.5)
 plt.title("Bar Chart of Ice-cream sales")
-plt.xlabel("Month")
-plt.ylabel("Sales(in thousand dollars)") 
+plt.xlabel('Month')
+plt.ylabel('Sales(in thousand dollars)') 
 plt.savefig("Ice-cream_sales.png")
