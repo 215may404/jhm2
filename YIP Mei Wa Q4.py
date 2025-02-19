e24627bcd5b977232f8198270319c1467e798778
215may404 <yipmay215@gmail.com>
2025-02-20 01:29:02 +0800

    Update YIP Mei Wa Q4.py

--- YIP Mei Wa Q4.py
+++ YIP Mei Wa Q4.py
@@ -1,34 +1,16 @@
-while True:
-    a = float(input("Input the height of a student 1 in cm:")),
-    if a < 0:
-        print("Height must be positive.")
-    elif a > 200:
-        print("Height is greater than 200cm.")
-    else:
-        print("Error")
-    b = float(input("Input the height of a student 2 in cm:")),
-    if b < 0:
-        print("Height must be positive.")
-    elif b > 200:
-        print("Height is greater than 200cm.")
-    else:
-        print("Error")
-    c = float(input("Input the height of a student 3 in cm:")),
-    if c < 0:
-        print("Height must be positive.")
-    elif c > 200:
-        print("Height is greater than 200cm.")
-    else:
-        print("Error")
-    d = float(input("Input the height of a student 4 in cm:")),
-    if d < 0:
-        print("Height must be positive.")
-    elif d > 200:
-        print("Height is greater than 200cm.")
-    else:
-        print("Error")
-        break
-values = [a,b,c,d]
+import numpy as np
+heights = []
+for i in range (1,5):
+    while True:
+        heights = float(input(f"Input the height of a student {i} in cm:"))
+        if (heights < 0):
+            print("Height must be positive.")
+        elif (heights > 200):
+            print("Height is greater than 200cm.")
+        else:
+            break
 print("End of input.")
-print("The average height of these students is {float(average(values))} cm.")
-print("The maximum height of these students is {float(max(values))} cm.")
+avg = np.average(round(heights,2))
+height = np.max(round(heights,2))
+print(f"The average height of these students is {avg} cm.")
+print(f"The maximum height of these students is {height} cm.")
